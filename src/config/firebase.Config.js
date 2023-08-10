ca284259dafdd61198d2ef81a8b38698e2e7ae3f
vvkrj18 <140914754+vvkrj18@users.noreply.github.com>
2023-08-10 22:55:44 +0530

    Add files via upload

---
+++ src/config/firebase.Config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJLd_jaNSWFCHijE4BcqVJMTolgUCVv4w",
+  authDomain: "cryptosolution-ae3f2.firebaseapp.com",
+  projectId: "cryptosolution-ae3f2",
+  storageBucket: "cryptosolution-ae3f2.appspot.com",
+  messagingSenderId: "481228976327",
+  appId: "1:481228976327:web:861e6b4f5196d64dbe0ee2",
+  measurementId: "G-L7DDGG6Y31"
+  };
+  export default firebaseConfig;
